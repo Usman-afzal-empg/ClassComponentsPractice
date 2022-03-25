1b6e4a6a8757cce9a97fce7f07f181a8f84af594
Usman Afzal <usmanafzal@Ali-Empg-MBP.local>
2022-03-25 12:45:51 +0500

    Functional component changed to class component

---
+++ src/components/example.jsx
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from 'react';
+
+class Example extends Component {
+    state = { 
+        name: '',
+        count : 0
+     };
+    render() {
+        const {name, count} = this.state;
+        return (
+            <Fragment>
+                <input type="text" value={name} onChange={this.handleNameChange}/>
+                {
+                    count > 0 && 
+                    <div>
+                        {name} has clicked {count} times
+                    </div>
+                }
+                
+                <button onClick={()=> this.setState({count: count+1})}>Increase</button>
+                <button onClick={()=> this.setState({count: count-1})}>Decrease</button>
+            </Fragment>
+        );
+    }
+    handleNameChange = event => {
+        const name = event.target.value;
+        this.setState({name});
+    }
+}
+export default Example;
