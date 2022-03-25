07e876302a1a1c21fd114f0127ca7d87132c9ace
Usman Afzal <usmanafzal@Ali-Empg-MBP.local>
2022-03-25 13:59:57 +0500

    File formattedd

--- src/components/example.jsx
+++ src/components/example.jsx
@@ -1,30 +1,36 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
 class Example extends Component {
-    state = { 
-        name: '',
-        count : 0
-     };
-    render() {
-        const {name, count} = this.state;
-        return (
-            <Fragment>
-                <input type="text" value={name} onChange={this.handleNameChange}/>
-                {
-                    count > 0 && 
-                    <div>
-                        {name} has clicked {count} times
-                    </div>
-                }
-                
-                <button onClick={()=> this.setState({count: count+1})}>Increase</button>
-                <button onClick={()=> this.setState({count: count-1})}>Decrease</button>
-            </Fragment>
-        );
-    }
-    handleNameChange = event => {
-        const name = event.target.value;
-        this.setState({name});
-    }
+  state = {
+    name: "",
+    count: 0,
+  };
+  componentDidMount() {
+    console.log("here");
+  }
+  render() {
+    const { name, count } = this.state;
+    return (
+      <Fragment>
+        <input type="text" value={name} onChange={this.handleNameChange} />
+        {count > 0 && (
+          <div>
+            {name} has clicked {count} times
+          </div>
+        )}
+
+        <button onClick={() => this.setState({ count: count + 1 })}>
+          Increase
+        </button>
+        <button onClick={() => this.setState({ count: count - 1 })}>
+          Decrease
+        </button>
+      </Fragment>
+    );
+  }
+  handleNameChange = (event) => {
+    const name = event.target.value;
+    this.setState({ name });
+  };
 }
 export default Example;
